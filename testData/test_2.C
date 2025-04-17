f9a17b28cb5bc491720ce39e514e0c452974b7ec
Rohanagrawal123 <115184991+Rohanagrawal123@users.noreply.github.com>
2025-04-18 00:30:06 +0530

    Update test_2.C

--- testData/test_2.C
+++ testData/test_2.C
@@ -1,8 +1,11 @@
 #include <stdio.h>
 int main() {
-    int x = 5;
-    float y = 3.14;
-    x = x + 1;
-    y = y * 2;
-    return 0;
+   // int x = 5;
+   // float y = 3.14;
+   // x = x + 1;
+   // y = y * 2;
+    //return 0;
+    int *x = malloc(10);
+    x = realloc(x, 20);
+    float *y = (float*) calloc(5, sizeof(float));
 }
