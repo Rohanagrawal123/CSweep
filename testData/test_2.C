8b8fb4da15f4c2e754909d2f1c677e64fae10a5e
Rohan Agrawal <115184991+Rohanagrawal123@users.noreply.github.com>
2025-05-24 00:51:30 +0530

    Update test_2.C

--- testData/test_2.C
+++ testData/test_2.C
@@ -1,11 +1,28 @@
 #include <stdio.h>
+#include <stdlib.h>
+
 int main() {
-   // int x = 5;
-   // float y = 3.14;
-   // x = x + 1;
-   // y = y * 2;
-    //return 0;
-    int *x = malloc(10);
-    x = realloc(x, 20);
-    float *y = (float*) calloc(5, sizeof(float));
+        int *ptr1 = (int *)malloc(sizeof(int));
+
+        for (int i = 0; i < 5; i++) {
+        int *ptr2 = (int *)malloc(sizeof(int));
+    }
+        int count = 0;
+    while (count < 3) {
+        float *ptr3 = (float *)malloc(sizeof(float));
+        count++;
+    }
+        char *ptr4;
+    int flag = 0;
+    do {
+        ptr4 = (char *)malloc(10 * sizeof(char));
+        flag++;
+    } while (flag < 2);
+
+        double *ptr5 = (double *)malloc(5 * sizeof(double));
+    ptr5 = (double *)realloc(ptr5, 10 * sizeof(double));
+
+        char *ptr6 = (char *)calloc(20, sizeof(char));
+
+    return 0;
 }
