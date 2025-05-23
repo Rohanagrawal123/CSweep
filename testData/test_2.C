7439467a56caf8864d95e1913edf4d35cf502e72
Rohan Agrawal <115184991+Rohanagrawal123@users.noreply.github.com>
2025-05-24 01:03:21 +0530

    Update test_2.C

--- testData/test_2.C
+++ testData/test_2.C
@@ -3,15 +3,19 @@
 
 int main() {
         int *ptr1 = (int *)malloc(sizeof(int));
+        ptr1 = (int *)malloc(sizeof(int)); // Reassigned without free
 
         for (int i = 0; i < 5; i++) {
         int *ptr2 = (int *)malloc(sizeof(int));
     }
+
         int count = 0;
     while (count < 3) {
         float *ptr3 = (float *)malloc(sizeof(float));
+        ptr3 = (float *)malloc(sizeof(float)); // Reassigned without free
         count++;
     }
+
         char *ptr4;
     int flag = 0;
     do {
@@ -20,7 +24,7 @@ int main() {
     } while (flag < 2);
 
         double *ptr5 = (double *)malloc(5 * sizeof(double));
-    ptr5 = (double *)realloc(ptr5, 10 * sizeof(double));
+        ptr5 = (double *)realloc(ptr5, 10 * sizeof(double)); // Reassigned without free
 
         char *ptr6 = (char *)calloc(20, sizeof(char));
 
